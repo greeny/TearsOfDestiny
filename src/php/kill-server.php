33bfed0f553835a814bb8678c0d8e71a2a08a4e0
greeny <blatny.tomas@seznam.cz>
2015-03-18 21:08:35 +0100

    Fixed kill script

--- src/php/kill-server.php
+++ src/php/kill-server.php
@@ -7,7 +7,7 @@ ob_start();
 system('ps aux | grep "server.php"');
 $grep = ob_get_clean();
 
-preg_match('#greeny\s+?([0-9]+).*php server\.php > /dev/null 2>/dev/null &#m', $grep, $matches);
+preg_match('#www-data\s+?([0-9]+).*php server\.php > /dev/null 2>/dev/null &#m', $grep, $matches);
 if (count($matches)) {
     system('kill ' . $matches[1]);
     echo " [INFO]: Server killed\n";
